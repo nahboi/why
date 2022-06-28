0aad1d58bd7336d1dbd4623140101e2742427116
Nah <55785917+nahboi@users.noreply.github.com>
2022-06-28 13:23:21 +0300

    fnf: commit

---
+++ source/base/ChartParser.hx
@@ -0,0 +1,164 @@
+package base;
+
+import AssetManager.EngineImplementation;
+import base.MusicSynced;
+import funkin.Note;
+import haxe.Json;
+import states.MusicBeatState;
+import states.PlayState;
+
+typedef SongFormat =
+{
+	var name:String;
+	var bpm:Float;
+	var events:Array<TimedEvent>;
+	var cameraEvents:Array<CameraEvent>;
+	var notes:Array<UnspawnedNote>;
+	var speed:Float;
+}
+
+typedef LegacySection =
+{
+	var sectionNotes:Array<Dynamic>;
+	var lengthInSteps:Int;
+	var typeOfSection:Int;
+	var mustHitSection:Bool;
+	var bpm:Float;
+	var changeBPM:Bool;
+	var altAnim:Bool;
+}
+
+typedef LegacySong =
+{
+	var song:String;
+	var notes:Array<LegacySection>;
+	var bpm:Float;
+	var needsVoices:Bool;
+	var speed:Float;
+	var player1:String;
+	var player2:String;
+	var stage:String;
+	var noteSkin:String;
+	var validScore:Bool;
+}
+
+class ChartParser
+{
+	public static var unspawnedNoteList:Array<Note> = [];
+	public static var difficultyMap:Map<Int, Array<String>> = [0 => ['-easy'], 1 => [''], 2 => ['-hard']];
+
+	public static function loadChart(state:MusicHandler, songName:String = 'test', difficulty:Int, songType:EngineImplementation = FNF_LEGACY):SongFormat
+	{
+		switch (songType)
+		{
+			case FNF:
+			// placeholder until new chart type 0.3
+			case FOREVER:
+
+			default:
+				var startTime:Float = Sys.time();
+				// pre 0.3 chart loader
+				var rawChart = AssetManager.getAsset(songName + difficultyMap[difficulty][0], JSON, 'songs/$songName');
+				var legacySong:LegacySong = cast Json.parse(rawChart).song;
+
+				// convert to standard format
+				var returnSong:SongFormat;
+				returnSong = {
+					name: legacySong.song,
+					bpm: legacySong.bpm,
+					events: [],
+					cameraEvents: [],
+					notes: [],
+					speed: legacySong.speed,
+				};
+
+				// load songs
+				var rawDirectory:String = AssetManager.getPath(songName, 'songs', DIRECTORY);
+				Conductor.bindSong(state, AssetManager.returnSound('$rawDirectory/Inst.ogg'), returnSong.bpm,
+					[AssetManager.returnSound('$rawDirectory/Voices.ogg')]);
+
+				// parse camera, note and events from legacy chart
+				for (i in 0...legacySong.notes.length)
+				{
+					// add it to the return song stuff
+					var cameraEvent:CameraEvent = {
+						stepTime: i * 16,
+						simple: true,
+						mustPress: legacySong.notes[i].mustHitSection
+					}
+					returnSong.cameraEvents.push(cameraEvent);
+
+					// push individual notes lmfao
+					for (j in legacySong.notes[i].sectionNotes)
+					{
+						if (j[1] >= 0)
+						{
+							var newNote:UnspawnedNote = {
+								stepTime: (j[0] / Conductor.stepCrochet),
+								// this formula sucks lmfao the base game format is ew
+								strumline: ((legacySong.notes[i].mustHitSection && j[1] <= 3 || !legacySong.notes[i].mustHitSection && j[1] > 3) ? 1 : 0),
+								index: Std.int(j[1] % 4),
+								type: 'default',
+								holdStep: (j[2] / Conductor.stepCrochet),
+								animationString: '',
+							}
+							returnSong.notes.push(newNote);
+						}
+						else
+						{
+							//
+						}
+					}
+				}
+
+				haxe.ds.ArraySort.sort(returnSong.notes, function(a, b):Int
+				{
+					return Std.int(a.stepTime - b.stepTime);
+				});
+
+				// psych events LOL
+				if (songType == PSYCH) {}
+
+				var endTime:Float = Sys.time();
+				trace('end chart parse time ${endTime - startTime}');
+				return returnSong;
+		}
+		return null;
+	}
+
+	/**
+	 * [Returns the events and notes of a loaded chart]
+	 * @param song The song to parse (in song format from loadChart)
+	 */
+	public static function parseChart(song:SongFormat):SongFormat
+	{
+		unspawnedNoteList = [];
+		for (unspawnNote in song.notes)
+		{
+			var newNote:Note = new Note(unspawnNote.stepTime, unspawnNote.index, unspawnNote.type, unspawnNote.strumline);
+			unspawnedNoteList.push(newNote);
+			if (unspawnNote.holdStep > 0)
+			{
+				var sustainLength = Std.int(unspawnNote.holdStep);
+				for (i in 1...sustainLength)
+				{
+					var newNote:Note = new Note(unspawnNote.stepTime + i, unspawnNote.index, unspawnNote.type, unspawnNote.strumline, true,
+						unspawnedNoteList[Std.int(unspawnedNoteList.length - 1)]);
+					unspawnedNoteList.push(newNote);
+				}
+			}
+			song.notes.splice(song.notes.indexOf(unspawnNote), 0);
+		}
+
+		// hold note bullshit
+
+		// sort notes
+		// /*
+		haxe.ds.ArraySort.sort(unspawnedNoteList, function(a, b):Int
+		{
+			return Std.int(a.stepTime - b.stepTime);
+		});
+		//  */
+		return song;
+	}
+}
---
+++ source/funkin/Note.hx
@@ -0,0 +1,139 @@
+package funkin;
+
+import base.Conductor;
+import base.ForeverDependencies.OffsettedSprite;
+import base.ScriptHandler.ForeverModule;
+import base.ScriptHandler;
+import funkin.Strumline.ReceptorData;
+import haxe.Json;
+import states.PlayState;
+
+class Note extends OffsettedSprite
+{
+	public var noteData:Int;
+	public var stepTime:Float;
+	public var strumline:Int = 0;
+	public var isSustain:Bool = false;
+	//
+	public var prevNote:Note;
+
+	// values
+	public var offsetX:Float = 0;
+	public var offsetY:Float = 0;
+
+	public var useCustomSpeed:Bool = false;
+	public var customNoteSpeed:Float;
+	public var noteSpeed(default, set):Float;
+
+	public function set_noteSpeed(value:Float):Float
+	{
+		if (noteSpeed != value)
+		{
+			noteSpeed = value;
+			updateSustainScale();
+		}
+		return noteSpeed;
+	}
+
+	public var tooLate:Bool = false;
+	public var canBeHit:Bool = false;
+	public var wasGoodHit:Bool = false;
+
+	public static var scriptCache:Map<String, ForeverModule> = [];
+	public static var dataCache:Map<String, ReceptorData> = [];
+
+	public var receptorData:ReceptorData;
+	public var noteModule:ForeverModule;
+
+	public function new(stepTime:Float, index:Int, noteType:String, strumline:Int, ?isSustain:Bool = false, ?prevNote:Note)
+	{
+		noteData = index;
+		this.stepTime = stepTime;
+		this.strumline = strumline;
+		this.isSustain = isSustain;
+		this.prevNote = prevNote;
+
+		super();
+
+		loadNote(noteType);
+	}
+
+	public function loadNote(noteType:String)
+	{
+		receptorData = returnNoteData(noteType);
+		noteModule = returnNoteScript(noteType);
+
+		noteModule.interp.variables.set('note', this);
+		// truncated loading functions by a ton
+		noteModule.interp.variables.set('getNoteDirection', getNoteDirection);
+		noteModule.interp.variables.set('getNoteColor', getNoteColor);
+
+		var generationScript:String = isSustain ? 'generateSustain' : 'generateNote';
+		if (noteModule.exists(generationScript))
+			noteModule.get(generationScript)();
+
+		// set note data stuffs
+		antialiasing = receptorData.antialiasing;
+		setGraphicSize(Std.int(frameWidth * receptorData.size));
+		updateHitbox();
+	}
+
+	public function updateSustainScale()
+	{
+		if (isSustain)
+		{
+			alpha = 0.6;
+			if (prevNote != null && prevNote.exists)
+			{
+				if (prevNote.isSustain)
+				{
+					// listen I dont know what i was doing but I was onto something
+					prevNote.scale.y = (prevNote.width / prevNote.frameWidth) * ((Conductor.stepCrochet / 100) * (1.07 / prevNote.receptorData.size)) * noteSpeed;
+					prevNote.updateHitbox();
+					offsetX = prevNote.offsetX;
+				}
+				else
+					offsetX = ((prevNote.width / 2) - (width / 2));
+			}
+		}
+	}
+
+	public static function returnNoteData(noteType:String):ReceptorData
+	{
+		// load up the note data
+		if (!dataCache.exists(noteType))
+		{
+			trace('setting note data $noteType');
+			dataCache.set(noteType, cast Json.parse(AssetManager.getAsset(noteType, JSON, 'notetypes/$noteType')));
+		}
+		return dataCache.get(noteType);
+	}
+
+	public static function returnNoteScript(noteType:String):ForeverModule
+	{
+		// load up the note script
+		if (!scriptCache.exists(noteType))
+		{
+			trace('setting note script $noteType');
+			scriptCache.set(noteType, ScriptHandler.loadModule(noteType, 'notetypes/$noteType'));
+		}
+		return scriptCache.get(noteType);
+	}
+
+	function getNoteDirection()
+		return receptorData.actions[noteData];
+
+	function getNoteColor()
+		return receptorData.colors[noteData];
+
+	override public function update(elapsed:Float)
+	{
+		if (stepTime > Conductor.stepPosition - (Conductor.msThreshold / Conductor.stepCrochet) //
+			&& stepTime < Conductor.stepPosition + (Conductor.msThreshold / Conductor.stepCrochet))
+			canBeHit = true;
+		else
+			canBeHit = false;
+
+		super.update(elapsed);
+	}
+}
